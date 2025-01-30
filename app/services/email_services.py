d485872e3439c4d85c9097b6c1562da1f966a3f9
Bratgans <bratgans@gmail.com>
2025-01-30 15:40:39 +0300

    change message with link

--- app/services/email_services.py
+++ app/services/email_services.py
@@ -1,5 +1,9 @@
 import asyncio
+import datetime
+import hashlib
 import smtplib
+import uuid
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -9,12 +13,36 @@ from app.config import config
 logger = get_logger()
 
 
+async def prepare_message(email: str, first_name: str | None, last_name: str | None) -> tuple[str, str]:
+    current_time = int(datetime.datetime.now().timestamp())
+    password_uuid = str(uuid.uuid4())[:10]
+    k_base_string = f"3ykOQzkL2X647dWw8dDx7h5c{email}{current_time}"
+    k_hash = hashlib.md5(k_base_string.encode()).hexdigest()
+
+    generated_link = (
+        f"{config.frontend.users_login_url}?un={email}"
+        f"&pw={password_uuid}&ln={last_name}"
+        f"&fn={first_name}&mn="
+        f"&g=&e={email}&t={current_time}"
+        f"&k={k_hash}"
+    )
+
+    subject = "Доступ к Личному кабинету MIP"
+    body = (
+        f"Вы получили доступ к Личному кабинету на сайте mip.institute. \n"
+        f"Перейдите по ссылке для его активации: {generated_link} \n"
+        f"\n"
+        f"\n"
+        f"С Уважением, \n"
+        f"Команда mip.institute"
+    )
+
+    return subject, body
+
+
 def send_email_sync(to_email: str, subject: str, body: str) -> bool:
     try:
-        smtp_server = "smtp.yandex.ru"
-        smtp_port = 587
-        smtp_user = "notify@mip.institute"
-        smtp_password = config.application.smtp_server_password
+        smtp_user = config.smtp.user
 
         msg = MIMEMultipart()
         msg["From"] = smtp_user
@@ -23,9 +51,9 @@ def send_email_sync(to_email: str, subject: str, body: str) -> bool:
 
         msg.attach(MIMEText(body, "plain"))
 
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
+        with smtplib.SMTP(config.smtp.server, config.smtp.port) as server:
             server.starttls()
-            server.login(smtp_user, smtp_password)
+            server.login(smtp_user, config.smtp.password)
             server.send_message(msg)
         return True
     except Exception as e:
